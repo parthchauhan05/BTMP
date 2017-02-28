1c38b27d3d0b9b48408495552cb6488198336909
Parth Chauhan <parthchauhan05@users.noreply.github.com>
2017-02-28 20:02:52 +0530

    Add files via upload

--- btmp.py
+++ btmp.py
@@ -4,6 +4,21 @@ from pymongo import *
 client = MongoClient('139.59.29.224',27017)
 app = Flask(__name__)
 
+@app.route('/get')
+def get():
+	t = 0
+	ans = dict()
+	u1 = str(request.args.get('u1'))
+	u2 = str(request.args.get('u2'))
+	login_db = client['msg']
+	login_collection = login_db['msg']
+	x = login_collection.find({'$or ': [{'$and' : [{'u1':u1 }, {'u2' : u2}]}, {'$and' :  [{'u1' : u2 }, {'u2' : u1 }]}]})
+	for i in x:
+		del i['_id']
+		ans = ans.update({t : i})
+		t += 1
+	return jsonify(ans)
+
 @app.route('/login')
 def login():
 	user = str(request.args.get('user'))
@@ -20,3 +35,4 @@ def main():
 if __name__ == '__main__':
 	app.secret_key = 'super secret key'
 	app.run(host='0.0.0.0', port=5612, debug=False)
+
