dea02a1f273b0f724dfdba3137d29ed718ad6d29
parthchauhan05 <parthchauhan05@gmail.com>
2017-03-01 01:51:04 +0530

    1 March 1:50 AM

--- btmp.py
+++ btmp.py
@@ -1,9 +1,45 @@
+'''Imported modules'''
 from flask import *
 from pymongo import *
+import time
 
+''' Some GLOBAL variables '''
 client = MongoClient('139.59.29.224',27017)
 app = Flask(__name__)
 
+''' Routine for feed '''
+@app.route('/feed')
+def feed():
+	
+''' Routine for deleting messege '''
+@app.route('/msgd')
+def msgd():
+	u1 = str(request.args.get('u1'))
+	u2 = str(request.args.get('u2'))
+	msg = str(request.args.get('msg'))
+	msg_db = client['msg']
+	msg_collection = msg_db['msg']
+	msg_collection.delete_one({'u1' : u1, 'u2' : u2,'msg' : msg})
+	return 'msg deleted'
+
+''' Routine for sending messeges '''
+@app.route('/msg')
+def msg():
+	u1 = str(request.args.get('u1'))
+	u2 = str(request.args.get('u2'))
+	msg = str(request.args.get('msg'))
+	msg_db = client['msg']
+	msg_collection = msg_db['msg']
+	data = {
+		'u1' : u1,
+		'u2' : u2,
+		'msg' : msg,
+		'time' : time.time()
+	}
+	msg_collection.insert_one(data)
+	return 'SENT'
+
+''' Routine for getting messeges '''
 @app.route('/get')
 def get():
 	t = 0
@@ -12,13 +48,16 @@ def get():
 	u2 = str(request.args.get('u2'))
 	login_db = client['msg']
 	login_collection = login_db['msg']
-	x = login_collection.find({'$or ': [{'$and' : [{'u1':u1 }, {'u2' : u2}]}, {'$and' :  [{'u1' : u2 }, {'u2' : u1 }]}]})
+	x = login_collection.find({'$or' : [{'$and' : [{'u1':u1 }, {'u2' : u2}]}, {'$and' :  [{'u1' : u2 }, {'u2' : u1 }]}]}).sort('time')
+	if x.count() == 0:
+		return 'NO CHAT FOUND'
 	for i in x:
 		del i['_id']
-		ans = ans.update({t : i})
+		ans[t] = i
 		t += 1
 	return jsonify(ans)
 
+''' Routine for login '''
 @app.route('/login')
 def login():
 	user = str(request.args.get('user'))
@@ -28,11 +67,13 @@ def login():
 	# print x
 	return jsonify(**x) if x is not None else "Bad"
 
+''' Home Page '''
 @app.route('/')
 def main():
 	return "<h3>Hello</h3>"
 
+''' Run main App '''
 if __name__ == '__main__':
 	app.secret_key = 'super secret key'
-	app.run(host='0.0.0.0', port=5612, debug=False)
+	app.run(host='0.0.0.0', port=5612, debug=True)
 
--- temp.py
+++ temp.py
@@ -1,10 +1,13 @@
 from pymongo import *
 import time
 client = MongoClient('139.59.29.224',27017)
-user = '201401140'
-db = client['msg']
-col = db['msg']
-col.insert_one({'u1' : '201401140', 'u2' : '201401139', 'time' : time.time(), 'msg': 'Sample messege'})
-col.insert_one({'u2' : '201401140', 'u1' : '201401139', 'time' : time.time(), 'msg': 'Reply to Sample messege'})
 
+db = client['login']
+col = db['login']
+data = {
+	'_id' : '201401139',
+	'Name' : 'Hardik Sardhara',
+	'password' : '00000000'
+}
+col.insert_one(data)
 print 'done'
