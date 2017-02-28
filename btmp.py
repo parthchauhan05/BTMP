1afb016857b49f77582774873f4a7aafc3701036
parthchauhan05 <parthchauhan05@gmail.com>
2017-03-01 01:55:35 +0530

    1 March

--- btmp.py
+++ btmp.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''Imported modules'''
 from flask import *
 from pymongo import *
@@ -41,14 +40,6 @@ def msg():
 	return 'SENT'
 
 ''' Routine for getting messeges '''
-=======
-from flask import *
-from pymongo import *
-
-client = MongoClient('139.59.29.224',27017)
-app = Flask(__name__)
-
->>>>>>> 1c38b27d3d0b9b48408495552cb6488198336909
 @app.route('/get')
 def get():
 	t = 0
@@ -57,7 +48,6 @@ def get():
 	u2 = str(request.args.get('u2'))
 	login_db = client['msg']
 	login_collection = login_db['msg']
-<<<<<<< HEAD
 	x = login_collection.find({'$or' : [{'$and' : [{'u1':u1 }, {'u2' : u2}]}, {'$and' :  [{'u1' : u2 }, {'u2' : u1 }]}]}).sort('time')
 	if x.count() == 0:
 		return 'NO CHAT FOUND'
@@ -68,15 +58,6 @@ def get():
 	return jsonify(ans)
 
 ''' Routine for login '''
-=======
-	x = login_collection.find({'$or ': [{'$and' : [{'u1':u1 }, {'u2' : u2}]}, {'$and' :  [{'u1' : u2 }, {'u2' : u1 }]}]})
-	for i in x:
-		del i['_id']
-		ans = ans.update({t : i})
-		t += 1
-	return jsonify(ans)
-
->>>>>>> 1c38b27d3d0b9b48408495552cb6488198336909
 @app.route('/login')
 def login():
 	user = str(request.args.get('user'))
@@ -86,22 +67,13 @@ def login():
 	# print x
 	return jsonify(**x) if x is not None else "Bad"
 
-<<<<<<< HEAD
 ''' Home Page '''
-=======
->>>>>>> 1c38b27d3d0b9b48408495552cb6488198336909
 @app.route('/')
 def main():
 	return "<h3>Hello</h3>"
 
-<<<<<<< HEAD
 ''' Run main App '''
 if __name__ == '__main__':
 	app.secret_key = 'super secret key'
 	app.run(host='0.0.0.0', port=5612, debug=True)
-=======
-if __name__ == '__main__':
-	app.secret_key = 'super secret key'
-	app.run(host='0.0.0.0', port=5612, debug=False)
->>>>>>> 1c38b27d3d0b9b48408495552cb6488198336909
 
